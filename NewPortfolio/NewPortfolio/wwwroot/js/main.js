642b72d963c1c1b787fa5d24a6d403f0ac69ce45
Fediuk Nicolas <nicolasfediuk@gmail.com>
2023-08-18 17:46:13 -0300

    start nav and header

---
+++ NewPortfolio/NewPortfolio/wwwroot/js/main.js
@@ -0,0 +1,47 @@
+﻿const navAction = document.querySelector(".menu");
+const NewNav = document.querySelector("nav");
+const h1 = document.querySelector("header");
+
+
+cargarEventListeners();
+
+function cargarEventListeners() {
+    
+    
+}
+
+function ViewNavResponsive(){
+
+    //if (window.getComputedStyle(navAction).getPropertyValue("display") == "none") {
+
+    //    console.log("entro");
+    //    const div = document.createElement("div");
+    //    div.classList.add("ContentNewNav");
+    //    div.innerHTML = `
+    //        <div>
+    //            <p>Nicolas Fediuk</p>
+    //        </div>
+
+    //        <div>
+    //            <p>menu</p>
+    //        </div>
+    //    `;
+    //    NewNav.appendChild(div);
+    //}
+}
+
+let mediaQuery = window.matchMedia("(max-width: 991px)");
+
+
+mediaQuery.addListener(function (mediaQueryList) {
+    //if (mediaQueryList.matches) {
+    //    ViewNavResponsive();
+    //}
+    //else {
+    //    const navResponsive = document.querySelector("nav .ContentNewNav");
+    //    if (navResponsive) {
+    //        navResponsive.remove();
+    //    }
+    //}
+});
+
